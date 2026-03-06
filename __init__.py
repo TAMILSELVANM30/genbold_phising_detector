61a949e4af481dbc23fd2c6d0fde0119549d9663
TAMILSELVANM30 <tam2006selvan@gmail.com>
2026-03-06 10:20:43 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,18 +1,6 @@
-from __future__ import annotations
+from ._dists import Distribution
+from ._envs import Environment
 
-from pip._internal.utils import _log
+__all__ = ["NAME", "Distribution", "Environment"]
 
-# init_logging() must be called before any call to logging.getLogger()
-# which happens at import of most modules.
-_log.init_logging()
-
-
-def main(args: list[str] | None = None) -> int:
-    """This is preserved for old console scripts that may still be referencing
-    it.
-
-    For additional details, see https://github.com/pypa/pip/issues/7498.
-    """
-    from pip._internal.utils.entrypoints import _wrapper
-
-    return _wrapper(args)
+NAME = "importlib"
